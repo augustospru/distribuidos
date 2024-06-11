1b840198b1c5cff3da5e2581cfeed322a606ff8d
augusto <augusto_prudencio@hotmail.com>
2024-06-11 15:55:27 -0300

    multi communication with threads

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 # Echo server program
 import socket
+import _thread as thread
+
+def on_new_client(conn: socket.socket, addr):
+    with conn:
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                continue
+            data = data + b' rcvd'
+            conn.sendall(data)
+        conn.close()
+
 
 HOST = 'localhost'                 # Symbolic name meaning all available interfaces
 PORT = 50007              # Arbitrary non-privileged port
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
-    s.listen(1)
-    conn, addr = s.accept()
-    with conn:
-        print('Connected by', addr)
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                print("a") 
-                break
-            conn.sendall(data)
+    s.listen(5)
+
+    while True:
+        conn, addr = s.accept()
+        print("new connection: ", addr)
+        thread.start_new_thread(on_new_client,(conn, addr))
--- test.py
+++ test.py
@@ -8,7 +8,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     i = 0
     while True:
         i += 1
-        s.sendall(b'Hello, world ' + str(i).encode())
+        s.sendall(b'AAAAAAAHello, world ' + str(i).encode())
         data = s.recv(1024)
         print('Received', repr(data))
         time.sleep(3)
